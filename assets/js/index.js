d3d55f6cf4aa6b3a9b5f48d13aef0c7ef82d3db1
lrubio013 <142952845+lrubio013@users.noreply.github.com>
2023-11-05 20:19:56 -0600

    added my questions and created function to initialize app

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,65 @@
 // TODO: Include packages needed for this application
-
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {  
+        type: "input",
+        name: "github",
+        message: "what is your github username?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "what is your email address?",
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "what is your project name?",
+    },
+    {
+        type: "input",
+        name: "Description",
+        message: "what is the description of you project?",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "what are the installation instructions of the project?",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "what is the usage of the project?",
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "How could a user help contribute to the project?",
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "How could we use the application to run tests?",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "what is the license of the project?",
+        choices: ["MIT","BSD", "Apache 2.0 license", "none"],
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt(questions)
+}
 
 // Function call to initialize app
 init();
